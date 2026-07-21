3b5d766ae05ff71792d69b6063e88243846e9288
Soumya9107 <soumyjitmaity31@gmail.com>
2026-07-21 23:16:21 +0530

    added resume section

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const resumeLists = document.querySelectorAll('.resume-list');
+const resumeBoxs = document.querySelectorAll('.resume-box');
 
 
-resumeLists.forEach((FileList, idx) => {
+resumeLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
         document.querySelector('.resume-list.active').classList.remove('active');
         list.classList.add('active');
+
+        document.querySelector('.resume-box.active').classList.remove('active');
+        resumeBoxs[idx].classList.add('active');
     });
 });
