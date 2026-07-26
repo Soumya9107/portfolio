6dada2f22a72403e924af9b92a8ab0f6b0de4a23
Soumya9107 <soumyjitmaity31@gmail.com>
2026-07-27 00:32:11 +0530

    added cube effects

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ navs.forEach((nav, idx) => {
 
         document.querySelector('.section.active').classList.remove('active');
         sections[idx].classList.add('active');
+
+        const array = Array.from(sections);
+        const arrSecs = array.slice(1, -1);
+        arrSecs.forEach(arrSec => {
+            if (arrSec.classList.contains('active')) {
+                sections[4].classList.add('action-contact');
+            }
+        });
+        if (sections[0].classList.contains('active')) {
+            sections[4].classList.remove('action-contact');
+        }
     });
 });
 
